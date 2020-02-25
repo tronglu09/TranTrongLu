6af7d51aa8de0dd51971470c5c7817bc2b91c17f
Trần Trọng Lư <trantronglu@gmail.com>
2020-02-25 11:38:00 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TTL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
